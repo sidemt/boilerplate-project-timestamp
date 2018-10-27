7808b498af920c57f324fa47667b877d80b95443
sidemt <25644062+sidemt@users.noreply.github.com>
2018-10-28 00:21:37 +0900

    Add timestamp API endpoint

--- server.js
+++ server.js
@@ -24,7 +24,24 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get('/api/timestamp/:date_string?', function(req, res) {
+  console.log(req.params.date_string);
+  var date = "";
+  if (req.params.date_string === undefined) {
+    console.log("date string is empty");
+    date = new Date();    
+  } else if (req.params.date_string.includes('-')) {
+    console.log("date string has - in it");
+    date = new Date(req.params.date_string);
+  } else {
+    console.log("date string does not have -");
+    var input = parseInt(req.params.date_string, 10);
+    // if date_string is not a number parseInt returns NaN
+    // new Date(NaN); returns Invalid Date
+    date = new Date(input);
+  }
+  res.json({"unix": date.getTime(), "utc": date.toUTCString()});
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
